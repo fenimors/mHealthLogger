019037812aa006bdc237411433de289f9af03f59
Sam Fenimore <fenimors@gmail.com>
2018-04-08 22:25:38 -0400

    starting to work

--- Learning/famouspeople/app/src/main/java/com/finepointmobile/famouspeople/CreateUser.java
+++ Learning/famouspeople/app/src/main/java/com/finepointmobile/famouspeople/CreateUser.java
@@ -1,4 +1,40 @@
 package com.finepointmobile.famouspeople;
 
-public class CreateUser {
+import android.nfc.Tag;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class CreateUser extends AppCompatActivity{
+
+    private static final String TAG = "CreateUser";
+
+    EditText firstname;
+    EditText lastname;
+    EditText email;
+    Button button;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+         super.onCreate(savedInstanceState);
+         setContentView(R.layout.create_user);
+
+         firstname = findViewById(R.id.first_name);
+         lastname = findViewById(R.id.last_name);
+         email = findViewById(R.id.email);
+         button = findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // TODO: Save to database
+                Log.d(TAG, "onClick: firstName: " + firstname.getText().toString());
+            }
+        });
+    }
 }
--- Learning/famouspeople/app/src/main/java/com/finepointmobile/famouspeople/MainActivity.java
+++ Learning/famouspeople/app/src/main/java/com/finepointmobile/famouspeople/MainActivity.java
@@ -1,5 +1,6 @@
 package com.finepointmobile.famouspeople;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -27,7 +28,8 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Log.d(TAG, "onCLick: pressed!");
+                //Log.d(TAG, "onCLick: pressed!");
+                startActivity(new Intent(MainActivity.this, CreateUser.class));
             }
         });
     }
